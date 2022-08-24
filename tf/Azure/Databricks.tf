59c6e7e3436740aba6cd6fd19d03eba225bea225
MoeinT <moin.torabi@gmail.com>
2022-08-24 21:13:33 +0200

    cluster to check policy set

--- tf/Azure/Databricks.tf
+++ tf/Azure/Databricks.tf
@@ -16,19 +16,19 @@ provider "databricks" {
 }
 
 #Standard_DS4_v2
-# resource "databricks_cluster" "SingleNodeCluster" {
-#   cluster_name            = "db-cluster-${var.suffix}-${var.environment}"
-#   spark_version           = "11.1.x-scala2.12"
-#   node_type_id            = "Standard_DS3_v2"
-#   autotermination_minutes = 10
-#   num_workers             = 0
+resource "databricks_cluster" "SingleNodeCluster" {
+  cluster_name            = "db-cluster-${var.suffix}-${var.environment}"
+  spark_version           = "11.1.x-scala2.12"
+  node_type_id            = "Standard_DS4_v2"
+  autotermination_minutes = 10
+  num_workers             = 0
 
-#   spark_conf = {
-#     "spark.databricks.cluster.profile" : "singleNode"
-#     "spark.master" : "local[*]"
-#   }
+  spark_conf = {
+    "spark.databricks.cluster.profile" : "singleNode"
+    "spark.master" : "local[*]"
+  }
 
-#   custom_tags = {
-#     "ResourceClass" = "SingleNode"
-#   }
-# }
+  custom_tags = {
+    "ResourceClass" = "SingleNode"
+  }
+}

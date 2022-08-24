9b3c1f7ff5c14f30ca8cb734d7f5c81776105b5d
MoeinT <moin.torabi@gmail.com>
2022-08-24 12:04:54 +0200

    added a fs (dev)

--- tf/Azure/Storages.tf
+++ tf/Azure/Storages.tf
@@ -11,3 +11,8 @@ resource "azurerm_storage_account" "sq-streamingData" {
   account_replication_type = "LRS"
   is_hns_enabled           = true
 }
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "fs-streamingData" {
+  name               = "commonfiles-${var.environment}"
+  storage_account_id = azurerm_storage_account.sq-streamingData.id
+}

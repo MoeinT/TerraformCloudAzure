723c1e9b1e7908d5e42157abb1d07cc351c83c9e
MoeinT <moin.torabi@gmail.com>
2022-08-24 15:10:05 +0200

    added a keyvault with access policies

--- tf/Modules.tf
+++ tf/Modules.tf
@@ -1,5 +1,7 @@
 module "Azure" {
-  source      = "./Azure"
-  project     = var.project
-  environment = var.environment
+  source       = "./Azure"
+  project      = var.project
+  environment  = var.environment
+  sp_object_id = var.sp_object_id
+  moein_obj_id = var.moein_obj_id
 }

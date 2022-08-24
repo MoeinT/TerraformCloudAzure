474a383e17606dce573201a34123a3e65ba28b0a
MoeinT <moin.torabi@gmail.com>
2022-08-24 09:53:40 +0200

    added sa

--- tf/Azure/Storages.tf
+++ tf/Azure/Storages.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "rg-streamingData" {
   name     = "rg-${var.suffix}-${local.env}"
   location = var.location
 }
+
+resource "azurerm_storage_account" "sq-streamingData" {
+  name                     = "sa22tfcloud${local.env}"
+  resource_group_name      = azurerm_resource_group.rg-streamingData.name
+  location                 = azurerm_resource_group.rg-streamingData.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+}

0773b1174c474d178deb5748f74dd1b727d3da9d
MoeinT <moin.torabi@gmail.com>
2022-08-24 10:03:26 +0200

    added a sa in dev

--- tf/Azure/Storages.tf
+++ tf/Azure/Storages.tf
@@ -3,11 +3,11 @@ resource "azurerm_resource_group" "rg-streamingData" {
   location = var.location
 }
 
-# resource "azurerm_storage_account" "sq-streamingData" {
-#   name                     = "sa22tfcloud${local.env}"
-#   resource_group_name      = azurerm_resource_group.rg-streamingData.name
-#   location                 = azurerm_resource_group.rg-streamingData.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   is_hns_enabled           = true
-# }
+resource "azurerm_storage_account" "sq-streamingData" {
+  name                     = "sa22tfcloud${local.env}"
+  resource_group_name      = azurerm_resource_group.rg-streamingData.name
+  location                 = azurerm_resource_group.rg-streamingData.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+}

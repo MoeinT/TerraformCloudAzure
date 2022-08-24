dba8219301b943e5c441e7a0e2f762804bdaf589
MoeinT <moin.torabi@gmail.com>
2022-08-24 17:09:00 +0200

    added a cluster

--- tf/Azure/Databricks.tf
+++ tf/Azure/Databricks.tf
@@ -14,3 +14,20 @@ provider "databricks" {
   azure_client_secret         = env.ARM_CLIENT_SECRET
   azure_tenant_id             = data.azurerm_client_config.current.tenant_id
 }
+
+resource "databricks_cluster" "SingleNodeCluster" {
+  cluster_name            = "db-cluster-${var.suffix}-${var.environment}"
+  spark_version           = "7.3.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 10
+  num_workers             = 0
+
+  spark_conf = {
+    "spark.databricks.cluster.profile" : "singleNode"
+    "spark.master" : "local[*]"
+  }
+
+  custom_tags = {
+    "ResourceClass" = "SingleNode"
+  }
+}
